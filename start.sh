1942b6d2a2eda3fc134f61044d002dba10ae9632
Candid Dauth <cdauth@cdauth.eu>
2018-01-04 22:27:33 +0100

    Bitpoll needs write access to CSS files

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ cd /bitpoll
 
 ./manage.py compilemessages
 rm -rf _static && ./manage.py collectstatic
+chmod a+w _static/scss/*.css
 
 exec su daemon -s/bin/sh -c "./manage.py runserver 0.0.0.0:8888"
